4cdc74c6d44bec7cd59952a366030ecf270b76a5
dninja777 <duroneppsjr@hotmail.com>
2016-08-23 23:57:02 +0000

    Fixed problems added model question

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822175238) do
+ActiveRecord::Schema.define(version: 20160823215952) do
 
   create_table "questions", force: :cascade do |t|
-    t.string   "email",      null: false
-    t.text     "body",       null: false
+    t.string   "email"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
