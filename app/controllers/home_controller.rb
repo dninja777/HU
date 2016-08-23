7bca11014201b3447b6dedee076735282404b81d
dninja777 <duroneppsjr@hotmail.com>
2016-08-23 15:59:15 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+  
+  def about
+  end
+  
+  def temp
+    redirect_to root_path
+  end
+  
+  
+end
